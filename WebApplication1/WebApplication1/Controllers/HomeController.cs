f6cab0e7631ac5ffd9fa7fe6cd1d9e08ce8b5ad4
unknown <=>
2019-03-12 02:31:04 +0800

    Initial commit on git

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly Models.IPersonRepository _personRepo;
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Sample MVC Core Ko 1.2";
+
+            var model = _personRepo.GetAllPerson().OrderBy(p => p.FirstName);
+
+            return View(model);
+        }
+
+        public HomeController(Models.IPersonRepository personRepo)
+        {
+            _personRepo = personRepo;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/MockPersonRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class MockPersonRepository : IPersonRepository
+    {
+        private List<Person> _person;
+
+        public IEnumerable<Person> GetAllPerson()
+        {
+            return _person;
+        }
+
+        public Person GetPersonById(string PersonId)
+        {
+            return _person.FirstOrDefault(p => p.PersonId == PersonId);
+        }
+
+
+        public MockPersonRepository()
+        {
+            if(_person == null)
+            {
+                InitializePerson();
+
+            }
+        }
+
+        private void InitializePerson()
+        {
+            _person = new List<Person>
+            {
+
+                 new Person { PersonId = "100001", LastName = "Angeles", FirstName = "Archimedes", Address = "San Isidro, Nueva Ecija"},
+
+                  new Person { PersonId = "100002", LastName = "Angeles", FirstName = "Alexander", Address = "San Isidro, Nueva Ecija"},
+
+                  new Person { PersonId = "100003", LastName = "Angeles", FirstName = "Anabelle", Address = "San Isidro, Nueva Ecija"}
+
+            };
+        }
+    }
+}
